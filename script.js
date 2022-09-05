e8c10d69c174594b7f76997bcdeca0f740f49525
Sam George Mathew <sam.mathew19113@gmail.com>
2022-09-05 15:42:57 +0530

    display operands on click

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 const form = document.getElementById("calc_form");
 
-form.addEventListener("submit",(e) => {
-    e.preventDefault();
-})
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+const output = document.getElementById("output");
+const operand_btns = document.querySelectorAll("button[data-type=operand]");
+let is_operator = false;
+operand_btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if (output.value == "0") {
+      output.value = e.target.value;
+    } else if (output.value.includes(".")) {
+      output.value = output.value + "" + e.target.value.replace(".", "");
+    } else if (is_operator) {
+      is_operator = false;
+      output.value = e.target.value;
+    } else {
+      output.value = output.value + "" + e.target.value;
+    }
+  });
+});
